2a17b3ca73d8febdad1f056a7d1aa567a1a4b9c0
Student <example@example.com>
2018-12-03 11:04:26 -0800

    add sorting feature to .all method of tasks

--- lib/task.rb
+++ lib/task.rb
@@ -1,26 +1,28 @@
 class Task
-  attr_reader(:description, :list_id, :due_date)
+  attr_reader(:description, :list_id, :due_date, :id)
 
   def initialize(attributes)
     @description = attributes.fetch("description")
     @due_date = attributes.fetch("due_date")
-    @list_id = attributes.fetch("id").to_i
+    @id = attributes.fetch("id").to_i
+    @list_id = attributes.fetch("list_id").to_i
   end
 
   def self.all
-    returned_tasks = DB.exec("SELECT * FROM tasks")
+    returned_tasks = DB.exec("SELECT * FROM tasks ORDER BY list_id DESC, due_date")
     tasks = []
-    returned_tasks.each() do |task_i|
-      tasks.push(Task.new(task_i))
+    returned_tasks.each() do |task|
+      tasks.push(Task.new(task))
     end
     tasks
   end
 
   def save
-    DB.exec("INSERT INTO tasks (id, description, due_date) VALUES (#{@list_id}, '#{@description}', '#{@due_date}');")
+    result = DB.exec("INSERT INTO tasks (description, due_date, list_id) VALUES ('#{@description}', '#{@due_date}', #{@list_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
   end
 
   def ==(another_task)
-  self.description().==(another_task.description()).&(self.due_date().==(another_task.due_date())).&(self.list_id().==(another_task.list_id()))
+  self.description().==(another_task.description()).&(self.due_date().==(another_task.due_date())).&(self.list_id().==(another_task.list_id())).&(self.id().==(another_task.id()))
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,4 +1,5 @@
 require('spec_helper')
+require('pry')
 
 describe(Task)do
   describe('.all')do
@@ -9,7 +10,7 @@ describe(Task)do
 
   describe('#save')do
     it('adds a task to the array of saved tasks') do
-      test_task = Task.new({'description' => 'learn SQL', 'id' => 1, 'due_date' => '1999-01-08 04:05:06'})
+      test_task = Task.new({'description' => 'learn SQL', 'list_id' => 1, 'due_date' => '1999-01-08 04:05:06', 'id' => nil})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
@@ -17,25 +18,36 @@ describe(Task)do
 
   describe('#description') do
     it('lets you read the description out')do
-      test_task = Task.new({'description' => 'learn SQL', 'id' => 1, 'due_date' => '1999-01-08 04:05:06'})
+      test_task = Task.new({'description' => 'learn SQL', 'list_id' => 1, 'due_date' => '1999-01-08 04:05:06', 'id' => nil})
       expect(test_task.description()).to(eq('learn SQL'))
     end
   end
 
   describe('#list_id') do
     it('lets you read the list ID out') do
-      test_task = Task.new({'description' => 'learn SQL', 'id' => 1, 'due_date' => '1999-01-08 04:05:06'})
+      test_task = Task.new({'description' => 'learn SQL', 'list_id' => 1, 'due_date' => '1999-01-08 04:05:06', 'id' => nil})
       expect(test_task.list_id()).to(eq(1))
     end
   end
 
   describe('#==') do
     it('is the same task if it has the same description') do
-      task1 = Task.new({'description' => 'learn SQL', 'id' => 1, 'due_date' => '1999-01-08 04:05:06'})
-      task2 = Task.new({'description' => 'learn SQL', 'id' => 1, 'due_date' => '1999-01-08 04:05:06'})
+      task1 = Task.new({'description' => 'learn SQL', 'list_id' => 1, 'due_date' => '1999-01-08 04:05:06', 'id' => nil})
+      task2 = Task.new({'description' => 'learn SQL', 'list_id' => 1, 'due_date' => '1999-01-08 04:05:06', 'id' => nil})
       expect(task1).to(eq(task2))
     end
   end
 
+  describe('.all') do
+    it('tests the sorting functionality of progsql') do
+      task1 = Task.new({'description' => 'learn SQL', 'list_id' => 1, 'due_date' => '1999-01-09 04:05:06', 'id' => nil})
+      task1.save
+      task2 = Task.new({'description' => 'learn SQL', 'list_id' => 1, 'due_date' => '1999-01-08 04:05:06', 'id' => nil})
+      task2.save
+      task3 = Task.new({'description' => 'learn SQL', 'list_id' => 1, 'due_date' => '1999-01-10 04:05:06', 'id' => nil})
+      task3.save
+      expect(Task.all).to(eq([task2, task1, task3]))
+    end
+  end
 
 end
