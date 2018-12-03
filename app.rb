1571312aab83ae1b514bb4ac2da7378a351d3d32
Student <example@example.com>
2018-12-03 15:20:21 -0800

    add ui homepage

--- app.rb
+++ app.rb
@@ -4,16 +4,22 @@ also_reload("lib/**/*.rb")
 require("./lib/Task")
 require("./lib/List")
 require("pg")
+require 'pry'
 
 DB = PG.connect({:dbname => "to_do"})
 
 get('/')do
+  DB.exec("DELETE FROM lists *;")
+  DB.exec("DELETE FROM tasks *;")
   @lists = List.all
   erb(:task)
 end
 
 post('/list')do
+  new_list = List.new(params)
+  new_list.save
   @lists = List.all
+  binding.pry
   erb(:task)
 end
 
