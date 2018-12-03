d25501c855d061b1fc5f703574452a5a3b1735d6
Paige Williams <paw145@humboldt.edu>
2018-12-02 21:44:11 -0800

    Add lists to database; all specs passing.

--- app.rb
+++ app.rb
@@ -5,13 +5,3 @@ require("./lib/task")
 require("pg")
 
 DB = PG.connect({:dbname => "to_do"})
-
-describe(Task) do
-  describe("#==") do
-    it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
-      expect(task1).to(eq(task2))
-    end
-  end
-end
--- lib/stage.rb
+++
@@ -1,8 +0,0 @@
-class Stage
-  def initialize(attributes)
-    @name = attributes.fetch(:name)
-    @location = attributes.fetch(:location)
-    @capacity = attributes.fetch(:capacity)
-    @artists = [ ]
-  end
-end
