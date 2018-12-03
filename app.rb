90eebf55fc1a44f7a5302c38d08f5b502974be48
Student <example@example.com>
2018-12-03 14:49:01 -0800

    commit to merge into master; tests passing

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
-require("./lib/task")
+require("./lib/Task")
+require("./lib/List")
 require("pg")
 
 DB = PG.connect({:dbname => "to_do"})
+
+get('/')do
+  @lists = List.all
+  erb(:task)
+end
+
+post('/list')do
+  @lists = List.all
+  erb(:task)
+end
+
+post('/list/:id') do
+  @lists = List.all
+  @tasks = Tasks.all
+  erb(:task)
+end
